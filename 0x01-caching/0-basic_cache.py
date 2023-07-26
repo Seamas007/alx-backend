15cab87f14f5a4659721939265e852924dbebf98
Seamas007 <olufy007@gmail.com>
2023-07-26 10:03:53 +0100

    My commit

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,52 +1,21 @@
 #!/usr/bin/env python3
-
+"""Basic caching module.
 """
-BasicCache Module
-"""
-
-class BaseCaching:
-    """
-    BaseCaching class
-    """
-    def __init__(self):
-        self.cache_data = {}
-
-    def print_cache(self):
-        """
-        Print the current cache items
-        """
-        print("Current cache:")
-        for key, value in self.cache_data.items():
-            print(f"{key}: {value}")
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """
-    BasicCache class, inherits from BaseCaching
+    """Represents an object that allows storing and
+    retrieving items from a dictionary.
     """
     def put(self, key, item):
+        """Adds an item in the cache.
         """
-        Put a key-value pair into the cache_data dictionary.
-
-        Args:
-            key: Key to be added.
-            item: Value to be associated with the key.
-
-        If key or item is None, this method should not do anything.
-        """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
+        """Retrieves an item by key.
         """
-        Get the value associated with the given key from cache_data.
-
-        Args:
-            key: Key to retrieve the value for.
-
-        Returns:
-            The value associated with the key or None if the key doesn't exist.
-        """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
