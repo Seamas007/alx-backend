b9123ae5cb5e64874177675054b2189ad444cf2f
Seamas007 <olufy007@gmail.com>
2023-07-26 10:07:50 +0100

    My commmit

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,61 +1,39 @@
 #!/usr/bin/env python3
-
+"""Least Recently Used caching module.
 """
-LRUCache Module
-"""
-
 from collections import OrderedDict
 
-class BaseCaching:
-    """
-    BaseCaching class
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        self.cache_data = {}
-
-    def print_cache(self):
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        error_msg = "put must be implemented in your cache class"
-        raise NotImplementedError(error_msg)
-
-    def get(self, key):
-        error_msg = "get must be implemented in your cache class"
-        raise NotImplementedError(error_msg)
+from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """
-    LRUCache class, inherits from BaseCaching
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LRU
+    removal mechanism when the limit is reached.
     """
     def __init__(self):
+        """Initializes the cache.
+        """
         super().__init__()
-        self.queue = OrderedDict()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                # Move the existing key to the end of the queue
-                self.queue.move_to_end(key)
-            else:
-                if len(self.cache_data) >= self.MAX_ITEMS:
-                    # Remove the least recently used item (first item in the queue)
-                    lru_key = next(iter(self.queue))
-                    self.cache_data.pop(lru_key)
-                    self.queue.popitem(last=False)
-                    print(f"DISCARD: {lru_key}")
-
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
             self.cache_data[key] = item
-            self.queue[key] = True
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        if key is None or key not in self.cache_data:
-            return None
-        # Move the accessed key to the end of the queue (most recently used)
-        self.queue.move_to_end(key)
-        return self.cache_data[key]
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
